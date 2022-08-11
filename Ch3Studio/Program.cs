474b96363e519216b59ea53be669379c6d8275ab
cwilliston <carolynnwilliston@gmail.com>
2022-08-10 19:57:55 -0500

    Add project files.

---
+++ Ch3Studio/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ch3Studio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringToCount;
+
+            Console.WriteLine("Enter a string: ");
+            stringToCount = Console.ReadLine().ToUpper();
+
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+            char[] charactersInString = stringToCount.ToCharArray();
+
+            foreach (char character in charactersInString)
+            {
+                if (charCount.ContainsKey(character))
+                {
+                    charCount[character]++;
+                } else
+                {
+                    charCount.Add(character, 1);
+                }
+            }
+
+            foreach (KeyValuePair<char, int> count in charCount)
+            {
+                Console.WriteLine($"{count.Key}: {count.Value}\n");
+            }
+
+        }
+    }
+}
